7475458bffe66da32667fb7766248fcc539e1807
Hope Barrow <barr.writing@gmail.com>
2022-07-10 00:08:04 -0500

    SAVING because I have messed up these files accidentally like 24 times already

--- assets/jovi.js
+++ assets/jovi.js
@@ -1,99 +1,137 @@
-console.log("test");
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
 
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progres')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-// const cover = document.querySelector('#natalie')
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const currTime = document.querySelector('#currTime');
+const durTime = document.querySelector('#durTime');
 
-// Song titles -- via array
-const songs = ['Talk', 'Almost (Sweet Music)', 'Howl', 'Bon, Bon', 'Superpower', 'For the Love of God', 'California Love (feat. Roger Troutman & Dr. Dre', 'Ghetto Gospel', 'The Bitch is Back', 'Vampire Money']
+// Song titles
+const songs = ['Talk', 'Almost (Sweet Music)', 'Howl', 'Bon, Bon', 'Superpower', 'For the Love of God', 'California Love', 'Ghetto Gospel', 'The Bitch is Back', 'Vampire Money'];
 
-//keep track of songs
-let songIndex = 9
+// Keep track of song
+let songIndex = 9;
 
-// Initially load songs info DOM
-loadSong(songs[songIndex])
+// Initially load song details into DOM
+loadSong(songs[songIndex]);
 
-//update song details
+// Update song details
 function loadSong(song) {
-    title.innerText = song
-    audio.src = `music/Talk.mp3`
-    audio.src = `music/Almost (Sweet Music).mp3`
-    audio.src = `music/Howl.mp3`
-    audio.src = `music/Bon,Bon.mp3`
-    audio.src = `music/Superpower.mp3`
-    audio.src = `music/For the Love of God.mp3`
-    audio.src = `music/California Love (feat. Roger Troutman & Dr. Dre.mp3`
-    audio.src = `music/Ghetto Gospel.mp3`
-    audio.src = `music/The Bitch is Back.mp3`
-    audio.src = `music/Vampire Money.mp3`
-    // cover.src = `albums/${song}.jpg`
+  title.innerText = song;
+  audio.src = `music/Talk.mp3`;
+//   audio.src = `music/Talk.mp3`;
+  audio.src = `music/Almost (Sweet Music).mp3`;
+//   audio.src = `music/Howl.mp3`;
+//   audio.src = `music/Bon, Bon.mp3`;
+//   audio.src = `music/Superpower.mp3`;
+//   audio.src = `music/For the Love of God.mp3`;
+//   audio.src = `music/California Love (feat. Roger Troutman & Dr. Dre).mp3`;
+//   audio.src = `music/Ghetto Gospel.mp3`;
+//   audio.src = `music/The Bitch is Back.mp3`;
+//   audio.src = `music/Vampire Money.mp3`;
+//   cover.src = `images/${song}.jpg`;
 }
 
-function playSong(){
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
+// Play song
+function playSong() {
+  musicContainer.classList.add('play');
+  playBtn.querySelector('i.fas').classList.remove('fa-play');
+  playBtn.querySelector('i.fas').classList.add('fa-pause');
 
-    audio.play()
+  audio.play();
 }
 
-function pauseSong(){
-    musicContainer.classList.remove('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+// Pause song
+function pauseSong() {
+  musicContainer.classList.remove('play');
+  playBtn.querySelector('i.fas').classList.add('fa-play');
+  playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
-    audio.pause()
+  audio.pause();
 }
 
+// Previous song
 function prevSong() {
-    songIndex--
+  songIndex--;
 
-if (songIndex <0) {
-    songIndex = songs.length - 1
+  if (songIndex < 0) {
+    songIndex = songs.length - 1;
+  }
+
+  loadSong(songs[songIndex]);
+
+  playSong();
 }
-    loadSong(songs[songIndex])
 
-    playSong()
-}
-
+// Next song
 function nextSong() {
-    songIndex ++ 
+  songIndex++;
 
-    if (songIndex > songs.length - 1){
-        songIndex = 0
-    }
+  if (songIndex > songs.length - 1) {
+    songIndex = 0;
+  }
 
-    loadSong(songs[songIndex])
+  loadSong(songs[songIndex]);
 
-    playSong()
-    
+  playSong();
 }
 
+// Update progress bar
 function updateProgress(e) {
-    const {duration, currentTime} = e.srcElement
-    const progressPrecent = (currentTime / duration) * 100
+  const { duration, currentTime } = e.srcElement;
+  const progressPercent = (currentTime / duration) * 100;
+  progress.style.width = `${progressPercent}%`;
 }
 
+// Set progress bar
+function setProgress(e) {
+  const width = this.clientWidth;
+  const clickX = e.offsetX;
+  const duration = audio.duration;
+
+  audio.currentTime = (clickX / width) * duration;
+}
+
+//get duration & currentTime for Time of song
+function DurTime (e) {
+	const {duration,currentTime} = e.srcElement;
+	var sec;
+	var sec_d;
+
+	// define minutes currentTime
+	let min = (currentTime==null)? 0:
+	 Math.floor(currentTime/60);
+	 min = min <10 ? '0'+min:min;
+}
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play')
+  const isPlaying = musicContainer.classList.contains('play');
 
-    if(isPlaying) {
-        pauseSong()
-    } else {
-        playSong()
-    }
-})
+  if (isPlaying) {
+    pauseSong();
+  } else {
+    playSong();
+  }
+});
 
-// change song events
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
+// Change song
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
-audio.addEventListener('timeupdate', updateProgress)
+// Time/song update
+audio.addEventListener('timeupdate', updateProgress);
+
+// Click on progress bar
+progressContainer.addEventListener('click', setProgress);
+
+// Song ends
+audio.addEventListener('ended', nextSong);
+
+// Time of song
+audio.addEventListener('timeupdate',DurTime);
--- assets/script.js
+++ assets/script.js
@@ -1,111 +1,104 @@
-console.log("test");
-
-const musicContainer = document.querySelector('.music-container')
-const playBtn = document.querySelector('#play')
-const prevBtn = document.querySelector('#prev')
-const nextBtn = document.querySelector('#next')
-const audio = document.querySelector('#audio')
-const progress = document.querySelector('.progres')
-const progressContainer = document.querySelector('.progress-container')
-const title = document.querySelector('#title')
-// const cover = document.querySelector('#natalie')
+const musicContainer = document.querySelector('.music-container');
+const playBtn = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio = document.querySelector('#audio');
+const progress = document.querySelector('.progress');
+const progressContainer = document.querySelector('.progress-container');
+const title = document.querySelector('#title');
+// const cover = document.querySelector('#heart');
 
 // Song titles -- via array
-const songs = ['Torn', 'Done.','Two Black Cadillacs', 'Blackbird', 'Gold', 'Location', 'Love in the Dark', 'Centuries', 'Cruise', 'Year 3000']
+const songs = ['Torn', 'Done.','Two Black Cadillacs', 'Blackbird', 'Gold', 'Location', 'Love in the Dark', 'Centuries', 'Cruise', 'Year 3000'];
 
 //keep track of songs
-let songIndex = 9
+let songIndex = 9;
 
 // Initially load songs info DOM
-loadSong(songs[songIndex])
+loadSong(songs[songIndex]);
 
 //update song details
 function loadSong(song) {
-    title.innerText = song
-    audio.src = `music/Torn.mp3`
-    audio.src = `music/Done..mp3`
-    audio.src = `music/Two Black Cadillacs.mp3`
-    audio.src = `music/Blackbird.mp3`
-    audio.src = `music/Gold.mp3`
-    audio.src = `music/Location.mp3`
-    audio.src = `music/Love in the Dark.mp3`
-    audio.src = `music/Centuries.mp3`
-    audio.src = `music/Cruise.mp3`
-    audio.src = `music/Year 3000.mp3`
-    // cover.src = `albums/${song}.jpg`
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
 }
 
 function playSong(){
-    musicContainer.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
 
     audio.play()
 }
 
 function pauseSong(){
-    musicContainer.classList.remove('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
 
-    audio.pause()
+    audio.pause();
 }
 
 function prevSong() {
-    songIndex--
+    songIndex--;
 
-if (songIndex <0) {
-    songIndex = songs.length - 1
+if (songIndex < 0) {
+    songIndex = songs.length - 1;
 }
-    loadSong(songs[songIndex])
+    loadSong(songs[songIndex]);
 
-    playSong()
+    playSong();
 }
 
 function nextSong() {
-    songIndex ++ 
+    songIndex++;
 
     if (songIndex > songs.length - 1){
-        songIndex = 0
+        songIndex = 0;
     }
 
-    loadSong(songs[songIndex])
+    loadSong(songs[songIndex]);
 
-    playSong()
-    
+    playSong();
 }
 
+//progress bar updates
 function updateProgress(e) {
-const {duration, currentTime} = e.srcElement
-const progressPrecent = (currentTime / duration) * 100
-progress.style.width = `${progressPrecent}%`
+const { duration, currentTime } = e.srcElement;
+const progressPrecent = (currentTime / duration) * 100;
+progress.style.width = `${progressPrecent}%`;
 
-function setProgress(e)
-    const width = this.clientWidth
-    const clickX = e.offsetX
-    const duration = audio.duration
 
-    audio.currentTime = (clickX / width) * duration
+//set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
 }
 
 
 // Event listeners
 playBtn.addEventListener('click', () => {
-    const isPlaying = musicContainer.classList.contains('play')
+    const isPlaying = musicContainer.classList.contains('play');
 
     if(isPlaying) {
-        pauseSong()
+        pauseSong();
     } else {
-        playSong()
+        playSong();
     }
-})
+});
 
 // change song events
-prevBtn.addEventListener('click', prevSong)
-nextBtn.addEventListener('click', nextSong)
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 
-audio.addEventListener('timeupdate', updateProgress)
+//time updates
+audio.addEventListener('timeupdate', updateProgress);
 
-progressContainer.addEventListener('click', setProgress)
+//progress bar clicks
+progressContainer.addEventListener('click', setProgress);
 
-audio.addEventListener('ended', nextSong)
+//time of song
+audio.addEventListener('ended', nextSong)};
